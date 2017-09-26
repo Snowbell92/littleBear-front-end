628946729a6227a26b3fb3c78278a0afec2078b3
Samia Ruponti <samia@bongobd.com>
2017-09-26 16:22:28 +0600

    fixed errors and ran build process again

--- src/showResults.js
+++ src/showResults.js
@@ -1,6 +1,5 @@
 import axios  from 'axios';
 import {BASE_URL} from './middleware/api';
-import jwt from 'jsonwebtoken';
 
 const token = localStorage.getItem('id_token');
 const AuthStr = 'Bearer '.concat(token);
@@ -9,29 +8,13 @@ let headers ={
     headers: { 'Content-Type':'application/json','Authorization' : AuthStr }
 };
 
-let isExpired = false;
-let decodedToken=jwt.decode(token, {complete: true});
-let dateNow = new Date();
-
-if(decodedToken.exp < dateNow.getTime())
-    isExpired = true;
 
 export default (async function showResults(values, dispatch) {
-   /* jwt.verify(token, 'shhhhh', { algorithm: 'public.pem'},function(err, decoded) {
-        if (!err) {
-
-    } else {
-        console.log(token);
-        console.log(err)
-    }
-    });*/
     axios.post(BASE_URL + 'human/new', values, headers)
         .then(function (response) {
-            console.log(isExpired)
             console.log(response);
         })
         .catch(function (error) {
-            console.log(isExpired)
             console.log(error.response);
         });
 });
